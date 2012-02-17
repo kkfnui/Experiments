f95c8679cc5fdf7e7a772e35b00e38adb0a64a61
kkfnui <kkfnui@126.com>
2012-02-17 12:56:31 +0800

    Add solution AboutNet.
    Query file information via WinInet lib.
    
    
    Signed-off-by: kkfnui <kkfnui@gmail.com>

---
+++ AboutNet/WinInet/MainDlg.h
@@ -0,0 +1,57 @@
+// MainDlg.h : interface of the CMainDlg class
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+#include <WinInet.h>
+#include "resource.h"
+
+class CMainDlg : public CDialogImpl<CMainDlg>
+{
+public:
+	enum { IDD = IDD_MAINDLG };
+
+	BEGIN_MSG_MAP(CMainDlg)
+		MESSAGE_HANDLER(WM_INITDIALOG, OnInitDialog)
+		COMMAND_ID_HANDLER(IDOK, OnOK)
+		COMMAND_ID_HANDLER(IDCANCEL, OnCancel)
+        COMMAND_HANDLER(ID_APP_ABOUT, BN_CLICKED, OnBnClickedAppAbout)
+    END_MSG_MAP()
+
+
+	LRESULT OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
+	{
+		// center the dialog on the screen
+		CenterWindow();
+
+		// set icons
+		HICON hIcon = (HICON)::LoadImage(_Module.GetResourceInstance(), MAKEINTRESOURCE(IDR_MAINFRAME), 
+			IMAGE_ICON, ::GetSystemMetrics(SM_CXICON), ::GetSystemMetrics(SM_CYICON), LR_DEFAULTCOLOR);
+		SetIcon(hIcon, TRUE);
+		HICON hIconSmall = (HICON)::LoadImage(_Module.GetResourceInstance(), MAKEINTRESOURCE(IDR_MAINFRAME), 
+			IMAGE_ICON, ::GetSystemMetrics(SM_CXSMICON), ::GetSystemMetrics(SM_CYSMICON), LR_DEFAULTCOLOR);
+		SetIcon(hIconSmall, FALSE);
+
+		return TRUE;
+	}
+
+
+	LRESULT OnOK(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+	{
+		// TODO: Add validation code 
+		EndDialog(wID);
+		return 0;
+	}
+
+	LRESULT OnCancel(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+	{
+		EndDialog(wID);
+		return 0;
+	}
+    LRESULT OnBnClickedAppAbout(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
+    BOOL GetDomainAndPortFromUrl(LPCTSTR strUrl, LPTSTR strDomain, INTERNET_PORT &nServerPort);
+
+private:
+    CString strUrl;
+    CString strFilename;
+};
