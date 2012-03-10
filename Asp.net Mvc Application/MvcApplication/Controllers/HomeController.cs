ffec996fe1e6ab05c9a8910b9bd28312dd50a801
kkfnui <kkfnui@gmail.com>
2012-03-11 00:30:03 +0800

    Modify the mvc  test. Signed-off-by: kkfnui <kkfnui@gmail.com>

--- Asp.net Mvc Application/MvcApplication/Controllers/HomeController.cs
+++ Asp.net Mvc Application/MvcApplication/Controllers/HomeController.cs
@@ -8,99 +8,20 @@ namespace MvcApplication.Controllers
 {
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
-
+        [HttpGet]
         public ActionResult Index()
         {
             ViewBag.Content = "Hello, this is my first asp.net mvc application.";
             return View();
         }
 
-        //
-        // GET: /Home/Details/5
-
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        //
-        // GET: /Home/Create
-
-        public ActionResult Create()
-        {
-            return View();
-        } 
-
-        //
-        // POST: /Home/Create
-
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Index(int id, string name)
         {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-        
-        //
-        // GET: /Home/Edit/5
- 
-        public ActionResult Edit(int id)
-        {
+            ViewData["id"] = id;
+            ViewData["name"] = name;
             return View();
         }
 
-        //
-        // POST: /Home/Edit/5
-
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
- 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //
-        // GET: /Home/Delete/5
- 
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        //
-        // POST: /Home/Delete/5
-
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
- 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
